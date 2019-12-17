bdeb23e070a92f84de504b570a6094fe5346275d
unknown <nstclair@ucmerced.edu>
2019-12-17 10:28:12 -0800

    Bug fixes.

--- acoustic_array/__init__.py
+++ acoustic_array/__init__.py
@@ -4,7 +4,7 @@ import struct
 
 
 class Controller(object):
-    def __init__(self, dev_id='1209:6130', bootloader_ids = ['1d50:6130', '1209:2100']):
+    def __init__(self, dev_id='1209:6130', bootloader_ids = ['1d50:6130', '1209:2100'], search_timeout=10):
         ports = get_ports(dev_id)
 
         if not ports:
@@ -16,21 +16,36 @@ class Controller(object):
                 if ports_bl:
                     print("Found a TinyFPGA in bootloader mode at %s... trying to reset." % id)
                     for p in ports_bl:
-                        p.write('\x00')
+                        p.write(b'\x00')
+                        p.close()
+
                         reset_some = True
 
-                time.sleep(0.5)
-
             if reset_some:
-                ports = tinyprog.get_ports(dev_id)
+                print("Searching for restarted device...")
+                for n in range(2*search_timeout):
+                    time.sleep(0.5)
+                    try:
+                        ports = get_ports(dev_id)
+                    except:
+                        ports = False
+                    if ports: break
+
                 if not ports:
                     raise RuntimeError('No devices found with address %s!' % dev_id)
                 else:
-                    print('That worked!')
+                    print('Found it!')
             else:
                 raise RuntimeError('No devices found with address %s, or TinyFPGAs in bootloader mode.' % id)
 
         self.port = ports[0]
+
+    def __enter__(self):
+        self.port.__enter__()
+        return self
+
+    def __exit__(self):
+        self.port.__exit__()
 
     def cmd(self, cmd, val1=0, val2=0):
         if type(cmd) is str: cmd = bytes(cmd, encoding='utf-8')
@@ -46,29 +61,39 @@ class Controller(object):
 # -----------------------------------------------------------------------------
 # The following functions are copied from TinyProg
 # (https://github.com/tinyfpga/TinyFPGA-Bootloader/tree/master/programmer)
+# They were modified to streamline things -- we just need to get a port to talk
 # TinyProg does not appear to have a license, so I guess it's ok?!
 # -----------------------------------------------------------------------------
-from pkg_resources import get_distribution, DistributionNotFound
-from serial.tools.list_ports import comports
-import serial
+
+# from pkg_resources import get_distribution, DistributionNotFound
+# from serial.tools.list_ports import comports
+# import serial
 import platform
 
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    __version__ = "unknown"
+# try:
+#     __version__ = get_distribution(__name__).version
+# except DistributionNotFound:
+#     # package is not installed
+#     __version__ = "unknown"
+#
+# try:
+#     from .full_version import __full_version__
+#     if not __full_version__:
+#         raise ValueError
+# except (ImportError, ValueError):
+#     __full_version__ = "unknown"
 
-try:
-    from .full_version import __full_version__
-    if not __full_version__:
-        raise ValueError
-except (ImportError, ValueError):
-    __full_version__ = "unknown"
 
+_use_libusb = False
 
-use_libusb = False
-use_pyserial = False
+if platform.system() == "Darwin" or _use_libusb:
+    import usb
+    _use_libusb = True
+
+else:
+    import serial
+    from serial.tools.list_ports import comports
+
 
 def get_ports(device_id):
     """
@@ -80,8 +105,7 @@ def get_ports(device_id):
 
     ports = []
 
-    if platform.system() == "Darwin" or use_libusb:
-        import usb
+    if _use_libusb:
         vid, pid = [int(x, 16) for x in device_id.split(":")]
 
         try:
@@ -94,8 +118,7 @@ def get_ports(device_id):
         except usb.core.USBError as e:
             raise PortError("Failed to open USB:\n%s" % str(e))
 
-    # MacOS is not playing nicely with the serial drivers for the bootloader
-    if platform.system() != "Darwin" or use_pyserial:
+    else:
         # get serial ports first
         ports += [
             SerialPort(p[0]) for p in comports() if device_id in p[2].lower()
@@ -113,32 +136,52 @@ class SerialPort(object):
         self.port_name = port_name
         self.ser = None
 
-    def __str__(self):
-        return self.port_name
-
-    def __enter__(self):
-        # Timeouts:
-        # - Read:  2.0 seconds (timeout)
-        # - Write: 5.0 seconds (writeTimeout)
-        #
-        # Rationale: hitting the writeTimeout is fatal, so it pays to be
-        # patient in case there is a brief delay; readTimeout is less
-        # fatal, but can result in short reads if it is hit, so we want
-        # a timeout high enough that is never hit normally.  In practice
-        # 1.0 seconds is *usually* enough, so the chosen values are double
-        # and five times the "usually enough" values.
-        #
         try:
             self.ser = serial.Serial(
                 self.port_name, timeout=2.0, writeTimeout=5.0).__enter__()
         except serial.SerialException as e:
             raise PortError("Failed to open serial port:\n%s" % str(e))
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __str__(self):
+        return self.port_name
+
+    # def __enter__(self):
+    #     # Timeouts:
+    #     # - Read:  2.0 seconds (timeout)
+    #     # - Write: 5.0 seconds (writeTimeout)
+    #     #
+    #     # Rationale: hitting the writeTimeout is fatal, so it pays to be
+    #     # patient in case there is a brief delay; readTimeout is less
+    #     # fatal, but can result in short reads if it is hit, so we want
+    #     # a timeout high enough that is never hit normally.  In practice
+    #     # 1.0 seconds is *usually* enough, so the chosen values are double
+    #     # and five times the "usually enough" values.
+    #     #
+    #     try:
+    #         self.ser = serial.Serial(
+    #             self.port_name, timeout=2.0, writeTimeout=5.0).__enter__()
+    #     except serial.SerialException as e:
+    #         raise PortError("Failed to open serial port:\n%s" % str(e))
+        #
+        # return self
+    #
+    # def __exit__(self, exc_type, exc_val, exc_tb):
+    #     try:
+    #         self.ser.__exit__(exc_type, exc_val, exc_tb)
+    #     except serial.SerialException as e:
+    #         raise PortError("Failed to close serial port:\n%s" % str(e))
+
+    def close(self, exc_type=None, exc_val=None, exc_tb=None):
         try:
             self.ser.__exit__(exc_type, exc_val, exc_tb)
         except serial.SerialException as e:
             raise PortError("Failed to close serial port:\n%s" % str(e))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type=None, exc_val=None, exc_tb=None):
+        self.close(exc_type=None, exc_val=None, exc_tb=None)
 
     def write(self, data):
         try:
--- examples/test.py
+++ examples/test.py
@@ -1,58 +1,74 @@
 import acoustic_array
+import sys
 
-if __name__ == '__main__':
-    # Open the phase controller
-    pc = acoustic_array.Controller()
+# Open the phase controller
+# with acoustic_array.Controller() as pc:
+pc = acoustic_array.Controller()
 
-    # The controller has 32 output banks (0-31), and 256 channels (0-255).
-    # Each pin on the output of the device controls 16 channels, i.e.
-    #   FPGA pin 1: channels 0-15
-    #   FPGA pin 2: channels 16-31
-    #   ... and so on.
-    # Additionally, pins 17-20 are the control outputs to the shift registers:
-    #   FPGA pin 17: data (DS or pin 14 on 74LVC595)
-    #   FPGA pin 18: shift clock (SHCP or pin 11 on 74LVC595)
-    #   FPGA pin 19: output register clock (STCP or pin 12 on 74LVC595)
-    # Each shift register only has 8 channels, however, you can get the full 16
-    #   on each output pin by connecting Q7S (pin 9) of one 74LVC595 to DS (pin
-    #   14) of the second.  SHCP and STCP should be connected to the same
-    #   channels on the first register.
-    # Note the additional requirements for each register:
-    #   GND (PIN 8)  => ground (G on FPGA)
-    #   ~MR (PIN 10) => 3.3 V (on FPGA)
-    #   ~OE (PIN 13) => ground
-    #   VCC (PIN 16) => 3.3 V
-    #
-    # At any given instance in time it is reading from one of the 32 `banks' to
-    #   produce the output.  You can also write to any of these banks at any
-    #   time; for example you might be reading from bank 1, then write to bank
-    #   2 and switch over once you're finished writing.
-    #
-    # All writes to the device take a command character and two 1 byte data
-    #   characters.  If the command character is capitalized, it will return
-    #   a response, otherwise it will not.
-    # The first response byte is an error code, which should always be 0.
-    # The second two bytes vary based on the command.
+# print(pc.port.ser)
 
-    # Here we will write to and then switch to bank 12, which is arbitrary.
-    bank = 12
 
-    # Select address to write to = bank, channel
-    # In this case we will write all 256 channels, so channel = 0
-    print('Write address:', pc.cmd('A', bank, 0))
+# The controller has 32 output banks (0-31), and 256 channels (0-255).
+# Each pin on the output of the device controls 16 channels, i.e.
+#   FPGA pin 1: channels 0-15
+#   FPGA pin 2: channels 16-31
+#   ... and so on.
+# Additionally, pins 17-20 are the control outputs to the shift registers:
+#   FPGA pin 17: data (DS or pin 14 on 74LVC595)
+#   FPGA pin 18: shift clock (SHCP or pin 11 on 74LVC595)
+#   FPGA pin 19: output register clock (STCP or pin 12 on 74LVC595)
+# Each shift register only has 8 channels, however, you can get the full 16
+#   on each output pin by connecting Q7S (pin 9) of one 74LVC595 to DS (pin
+#   14) of the second.  SHCP and STCP should be connected to the same
+#   channels on the first register.
+# Note the additional requirements for each register:
+#   GND (PIN 8)  => ground (G on FPGA)
+#   ~MR (PIN 10) => 3.3 V (on FPGA)
+#   ~OE (PIN 13) => ground
+#   VCC (PIN 16) => 3.3 V
+#
+# At any given instance in time it is reading from one of the 32 `banks' to
+#   produce the output.  You can also write to any of these banks at any
+#   time; for example you might be reading from bank 1, then write to bank
+#   2 and switch over once you're finished writing.
+#
+# All writes to the device take a command character and two 1 byte data
+#   characters.  If the command character is capitalized, it will return
+#   a response, otherwise it will not.
+# The first response byte is an error code, which should always be 0.
+# The second two bytes vary based on the command.
 
-    for n in range(256):
-        # Both phase and duty cycle range from 0-255, thus 128 is 50% duty
+# Here we will write to and then switch to bank 12, which is arbitrary.
+bank = 12
 
-        # Each channel advances by 8 in phase -- smaller changes won't be
-        #  visible, as the default driver has only 32 updates per cycle.
-        phase = (n * 8) % 256
-        # 50% duty cycle
-        duty = 128
+# Select address to write to = bank, channel
+# In this case we will write all 256 channels, so channel = 0
+print('Write address:', pc.cmd('A', bank, 0))
 
-        # This command writes to the current address, and updates the address by one.
-        # The returned data is the bank/channel just written.
-        print('Sucessfully wrote data to:', pc.cmd('W', phase, duty))
+try:
+    n0 = int(sys.argv[1])
+except:
+    n0 = 0
 
-    # Select the output bank
-    print('Selected bank:', pc.cmd('B', 0, bank))
+for n in range(32):
+    # Both phase and duty cycle range from 0-255, thus 128 is 50% duty
+
+    # Each channel advances by 8 in phase -- smaller changes won't be
+    #  visible, as the default driver has only 32 updates per cycle.
+    phase = (n * 8) % 256
+    # 50% duty cycle
+    # duty = 128
+    # duty = 128 if n == 0 else 8
+    if n == n0:
+        duty = 192
+    elif n == (n0+1):
+        duty = 64
+    else:
+        duty = 8
+
+    # This command writes to the current address, and updates the address by one.
+    # The returned data is the bank/channel just written.
+    print('Sucessfully wrote data to:', pc.cmd('W', phase, duty))
+
+# Select the output bank
+print('Selected bank:', pc.cmd('B', 0, bank))
